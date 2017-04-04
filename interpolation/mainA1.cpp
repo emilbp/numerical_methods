a28b71f1a10bb63e6bf8a841476336bf6cec9d1f
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-04-04 14:57:59 +0200

    Updated Makefile to produce graphs

--- interpolation/mainA1.cpp
+++ interpolation/mainA1.cpp
@@ -17,16 +17,18 @@ for (int i = 0; i < nx; i++) {
 	//x[i] = i + 0.5 * sin(i);
 	//y[i] = i + cos(i * i);
 	x[i] = i / M_PI;
-	y[i] = cos(x[i] * i);
+	y[i] = cos(x[i]);
 }
 
 // Output sampled values
+cout << "# m=0, S=4\n";
 for(size_t i=0;i<x.size();i++) cout << x[i] << " " << y[i] << endl;
 
 auto ls = lspline(x, y);
 
-int N = 20; double step = (x.back() - x.front()) / (N - 1);
+int N = 200; double step = (x.back() - x.front()) / (N - 1);
 
+cout << "# m=1, S=0\n";
 for(double z = x.front(); z < x.back(); z += step) {
 	cout << z << " " << ls(z) << endl;
 }
