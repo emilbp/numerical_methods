b95152ad7cab0c572f861acc5fa3853633df9fe4
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-04-06 09:12:01 +0200

    Updated function plotted

--- interpolation/mainA1.cpp
+++ interpolation/mainA1.cpp
@@ -17,14 +17,16 @@ vector<double> x(nx), y(nx), x_real(kx), y_real(kx);
 // Generate sampled values
 for (int i = 0; i < nx; i++) {
 	x[i] = i;
-	y[i] = i * pow(cos(i), 2);
+//	y[i] = 0.1 * i + pow(cos(3*i), 1);
+	y[i] = pow(cos(i*2),2) - 0.2;
 }
 
 // Generate 'real' values
 for (int i = 0; i < kx; i++) {
 	k = ((double)i * (double)nx) / (double)kx;
 	x_real[i] = k;
-	y_real[i] = k * pow(cos(k), 2);
+//	y_real[i] = 0.1 * k + pow(cos(3*k), 1);
+	y_real[i] = pow(cos(k*2),2) - 0.2;
 }
 
 // Output sampled values
@@ -45,7 +47,7 @@ for(double z = x.front(); z < x.back(); z += step) {
 	cout << z << " " << ls(z,0) << endl;
 }
 
-cout << "# m=2, S=0\n";
+cout << "# m=3, S=0\n";
 for(double z = x.front(); z < x.back(); z+=step) {
 	cout << z << " " << ls(z,1) << endl;
 }
