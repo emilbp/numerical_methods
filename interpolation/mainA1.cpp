273689a973064947ee6ff7ec234570947c7e3ba9
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-04-06 09:52:15 +0200

    Updated lspline function

--- interpolation/mainA1.cpp
+++ interpolation/mainA1.cpp
@@ -16,17 +16,15 @@ vector<double> x(nx), y(nx), x_real(kx), y_real(kx);
 
 // Generate sampled values
 for (int i = 0; i < nx; i++) {
-	x[i] = i;
-//	y[i] = 0.1 * i + pow(cos(3*i), 1);
-	y[i] = pow(cos(i*2),2) - 0.2;
+	x[i] = (double)i/2;
+	y[i] = pow(x[i],2) - 1;
 }
 
 // Generate 'real' values
 for (int i = 0; i < kx; i++) {
 	k = ((double)i * (double)nx) / (double)kx;
-	x_real[i] = k;
-//	y_real[i] = 0.1 * k + pow(cos(3*k), 1);
-	y_real[i] = pow(cos(k*2),2) - 0.2;
+	x_real[i] = k/2;
+	y_real[i] = pow(x_real[i],2) - 1;
 }
 
 // Output sampled values
