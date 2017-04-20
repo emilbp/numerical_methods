02c9a6d1e1df6a7edd0fe7e12759a79fba24b064
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-04-20 20:45:08 +0200

    Updated and cleaned up Jacobi Eigenvalue Algorithm

--- eigen/jacobi_eigen.cpp
+++ eigen/jacobi_eigen.cpp
@@ -4,15 +4,16 @@
 using namespace arma;
 
 int jacobi_eigen(mat& A, mat& V, vec& e) {
-int n = A.n_cols, sweeps = 0;
-bool changed = true;
+int n = A.n_cols, runs = 0, r1, c1, r2, c2;
+bool not_converged = true;
 e = A.diag();
 V.eye();
 
-while(changed){
-sweeps++;
+while(not_converged){
+runs++;
+not_converged = false;
 
-for (int q = n-1; q>0; q--) {		// q sweeps the columns
+for (int q = 0; q<n; q++) {		// q sweeps the columns
 	for (int p = 0; p<q; p++) {	// p sweeps the rows
 		double app = e(p);
 		double aqq = e(q);
@@ -24,41 +25,34 @@ for (int q = n-1; q>0; q--) {		// q sweeps the columns
 
 		// Limit criterion - if the value changed, update the entry in the matrix
 		if (app_new != app || aqq_new != aqq) {
-			changed = true;
+			not_converged = true;
 			e(p) = app_new;
 			e(q) = aqq_new;
-			A(p,q) = 0.0;
+			A(p,q) = 0;
 
-			for (int i = 0; i<p; i++) {
-				double aip = A(i,p);
-				double aiq = A(i,q);
-				A(i,p) = c*aip - s*aiq;
-				A(i,q) = s*aip + c*aiq;
+			for (int i = 0; i < n; i++) {
+				if (i != q && i != p) {
+					if (i < p) {r1 = i; c1 = p;} else {r1 = p; c1 = i;}
+					if (i < q) {r2 = i; c2 = q;} else {r2 = q; c2 = i;}
+
+				double aip = A(r1, c1);
+				double aiq = A(r2, c2);
+				A(r1, c1) = c*aip - s*aiq;
+				A(r2, c2) = c*aiq + s * aip;
 			}
-			for (int i = p+1; i<q; i++) {
-				double api = A(p,i);
-				double aiq = A(i,q);
-				A(p,i) = c*api - s*aiq;
-				A(i,q) = c*aiq + s*api;
+
+			double vip = V(i,p);
+			double viq = V(i,q);
+			V(i,p) = c*vip - s*viq;
+			V(i,q) = c*viq + s*vip;
+
 			}
-			for (int i = q+1; i<n; i++) {
-				double api = A(p,i);
-				double aqi = A(q,i);
-				A(p,i) = c*api - s*aqi;
-				A(q,i) = c*aqi + s*api;
-			}
-			for (int i = 0; i<n; i++) {
-				double vip = V(i,p);
-				double viq = V(i,q);
-				V(i,p) = c*vip - s*viq;
-				V(i,q) = s*vip + c*viq;
-			}
-		} else { changed = false; }
+		}
 	}
 
 }
 
 }
 
-return sweeps;
+return runs;
 }
