c5d139e9e399c37897125e36299f5633c87873ef
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-05-08 15:54:59 +0200

    ODE working with 12, 23 and 45

--- ode/mainA.cpp
+++ ode/mainA.cpp
@@ -4,27 +4,50 @@
 using namespace arma;
 using namespace std;
 
-tuple<vec, double> rkstep12(function<vec(vec, vec)> F, vec t, vec y, double h);
-tuple<vec, vec> rkdriver(function<vec(vec,vec)> F, vec ystart, vec time, double h, double eps, double acc);
+void rkdriver(
+	function<vec(double,vec)> & F,
+	vector<double> & xlist, vector<vec> & ylist,
+	string mode,
+	double  b, double h, double acc, double eps);
 
 int main() {
-
-	function<vec(vec, vec)> f1 = [] (vec x, vec y) {
-		int n = x.size(); vec z(n);
-		z[0] = -y[1]; z[1] = y[0];
-		return z;
+	function<vec(double, vec)> f1 = [] (double x, vec y) {
+		int n = y.size(); vec dydx(n);
+		dydx[0] = y[1];
+		dydx[1] = -y[0];
+		return dydx;
 	};
 
+	vector<double> xlist(1);
+	vector<vec> ylist(1);
+	double pi = 3.141592;
+	double a = 0, b = 2*pi, h = 0.01, acc = 0.01, eps = 0.01, shift = 0.05;
 
-	vec time = {0, 3}, ystart = {0, 10};
+	xlist[0] = a; ylist[0] = vec({0,1});
+	rkdriver(f1, xlist, ylist, "ode12", b, h, acc, eps);
 
-	time.print("x"); ystart.print("y");
-	f1(time,ystart).print("f(x,y)");
+	cout << "\"ode12: y2\" \"ode12: y2\"" << endl;
+	for(int i=0;i<xlist.size();i++) {
+		cout << xlist[i] << " " << ylist[i][0] << " " << ylist[i][1] << endl;
+	}
 
-	vec t; double y;
-//	tie(t, y) = rkdriver(f1, ystart, time, 0.01, 0.01, 0.01);
 
-	t.print("t is");
-	cout << "y is " << y << endl;
+	xlist[0] = a; ylist[0] = vec({0,1});
+	rkdriver(f1, xlist, ylist, "ode32", b, h, acc, eps);
+
+	cout << endl << endl << "\"ode32: y1\" \"ode32: y2\"" << endl;
+	for(int i=0;i<xlist.size();i++) {
+		cout << xlist[i] << " " << ylist[i][0] - shift << " " << ylist[i][1] - shift << endl;
+	}
+
+	xlist[0] = a; ylist[0] = vec({0,1});
+	rkdriver(f1, xlist, ylist, "ode45", b, h, acc, eps);
+
+	cout << endl << endl << "\"ode45: y1\" \"ode45: y2\"" << endl;
+	for(int i=0;i<xlist.size();i++) {
+		cout << xlist[i] << " " << ylist[i][0] + shift << " " << ylist[i][1] + shift << endl;
+	}
+
+
 return 0;
 }
--- ode/ode.cpp
+++ ode/ode.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 using namespace arma;
 
-tuple<vec, double> rkstep12(function<vec(vec,vec)> F, vec t, vec y, double h) {
+tuple<vec, double> rkstep12(function<vec(double,vec)> F, double t, vec y, double h) {
 	int n = y.size();
 
-	vec yt(n), yh(n), err(n);
+	vec yt = zeros(n), yh = zeros(n), err = zeros(n);
 	vec k0 = F(t,y);
 	for (int i = 0; i < n; i++) {
 		yt[i] = y[i] + k0[i] * h/2;
@@ -14,7 +14,7 @@ tuple<vec, double> rkstep12(function<vec(vec,vec)> F, vec t, vec y, double h) {
 
 	vec k12 = F(t + h/2, yt);
 	for (int i = 0; i < n; i++) {
-		yh[i] = y[i] + k12[i] * h/2;
+		yh[i] = y[i] + k12[i] * h;
 	}
 
 	for (int i = 0; i < n; i++) {
@@ -24,37 +24,99 @@ tuple<vec, double> rkstep12(function<vec(vec,vec)> F, vec t, vec y, double h) {
 return make_tuple(yh, norm(err));
 }
 
-
-tuple<vec, vec> rkdriver(function<vec(vec,vec)> F, vec ystart, vec time, double h, double eps, double acc) {
-	int n = ystart.size();
-	uvec indices(2);
-	for (int i = 0; i < n-1; i++) indices(i) = i;
-	vec ys = ystart, ts = time.elem(indices);
-	bool end = false;
-	while (end == false) {
-		double t = ts(ts.size()-1);
-		double y = ys(ys.size()-1);
-
-		if (t >= time(time.size()-1)) end = true;
-
-		if (t+h > time(time.size()-1)) h = time(time.size()-1) - t;
-
-		vec yh; double err;
-		tie(yh, err) = rkstep12(F, t, y, h);
-
-		double tol = (acc + norm(yh)*eps) * sqrt(h / (time(time.size()-1) - time(0)));
-
-		if (err < tol) {
-			ts.resize(ts.size()+1);
-			ts(ts.size()) = t+h;
-			ys.resize(ys.size()+1);
-			ys(ys.size()) = yh;
-		}
-		if (err == 0) {
-			h *= 2;
-		} else {
-			h *= pow(tol/err, 0.25) * 0.95;
-		}
+tuple<vec, double> rkstep32(function<vec(double,vec)> F, double t, vec y, double h) {
+	int n = y.size();
+	vec yt = zeros(n), ye = zeros(n), err = zeros(n);
+	vec k1(n), k2(n), k3(n), k4(n);
+	k1 = F(t,y);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (1/2) * k1[i] * h;
 	}
-return make_tuple(ts, ys);
+	k2 = F(t + h/2, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (3/4) * k2[i] * h;
+	}
+
+	k3 = F(t + 3/4 * h, yt);
+	for (int i = 0; i < n; i++) {
+		ye[i] = y[i] + ((2/9) * k1[i] + (1/3) * k2[i] + (4/9) * k3[i]) * h;
+	}
+
+	k4 = F(t + h, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + ((7/24) * k1[i] + (1/4) * k2[i] + (1/3) * k3[i] + (1/8) * k4[i]) * h;
+		err[i] = ye[i] - yt[i];
+	}
+
+return make_tuple(ye, norm(err));
+}
+
+tuple<vec, double> rkstep45(function<vec(double,vec)> F, double t, vec y, double h) {
+	int n = y.size();
+	vec yt = zeros(n), ye = zeros(n), err = zeros(n);
+	vec k1 = F(t,y);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + 1/4 * k1[i] * h;
+	}
+
+	vec k2 = F(t + h/4, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (3/32 * k1[i] + 9/32 * k2[i]) * h;
+	}
+
+	vec k3 = F(t + 3/8 * h, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (1932/2197 * k1[i] -7200*2197 * k2[i] + 7296/2197 * k3[i]) * h;
+	}
+
+	vec k4 = F(t + 12/13 * h, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (439/213 * k1[i] -8 * k2[i] + 3680/513 * k3[i] - 845/4104 * k4[i]) * h;
+	}
+
+	vec k5 = F(t + h, yt);
+	for (int i = 0; i < n; i++) {
+		yt[i] = y[i] + (-8/27 * k1[i] + 2 * k2[i] - 3544/2565 * k3[i] + 1859/4104 * k4[i] - 11/40 * k5[i]) * h;
+	}
+
+	vec k6 = F(t + h/2, yt);
+
+	for (int i = 0; i < n; i++) {
+		ye[i] = y[i] + (16/135 * k1[i] + 6656/12825 * k3[i] + 28561/56430 * k4[i] - 9/50 * k5[i] + 2/55 * k6[i]) * h;
+		yt[i] = y[i] + (25/216 * k1[i] - 1408/2565 * k3[i] + 2197/4104 * k4[i] - 1/5 * k5[i]) * h;
+		err[i] = ye[i] - yt[i];
+	}
+return make_tuple(ye, norm(err));
+}
+
+
+void rkdriver(
+	function<vec(double,vec)> & F,
+	vector<double> & xlist, vector<vec> & ylist,
+	string mode,
+	double b, double h, double acc, double eps) {
+
+	int n = ylist.front().size();
+	double a = xlist.front();
+	vec y1(n); double err;
+
+	while(xlist.back() < b) {
+		double x = xlist.back();
+		vec y = ylist.back();
+		if (x+h > b) h = b-x;
+		if (mode == "ode12") {tie(y1, err) = rkstep12(F, x, y, h);}
+		else if (mode == "ode32") {tie(y1, err) = rkstep32(F, x, y, h);}
+		else if (mode == "ode45") {tie(y1, err) = rkstep45(F, x, y, h);}
+		else {throw invalid_argument("Stepper mode not recognized");}
+
+		double tol = (acc + norm(y1,2)*eps)*sqrt(h/(b-a));
+
+		if (tol > err) {
+			xlist.push_back(x+h);
+			ylist.push_back(y1);
+		}
+		if (err > 0) {
+			h = h*pow(tol/err,0.25)*0.95;
+		} else {h = 2*h;}
+	}
 }
