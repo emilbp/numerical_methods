a1779f944635750304a943cd868536cf4e2987f9
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-05-09 18:08:47 +0200

    Open and closed form working

--- integration/adapt.cpp
+++ integration/adapt.cpp
@@ -3,18 +3,19 @@
 #include <iostream>
 using namespace std;
 
-double integ_open(function<double(double)> f, double a, double b, double f2, double f3, double acc, double eps);
+double integ_open(function<double(double, int)> f, int p, double a, double b, double f2, double f3, double acc, double eps);
+double integ_closed(function<double(double, int)> f, int p, double a, double b, double f1, double f3, double f5, double acc, double eps);
 
-double adapt(function<double(double)> f, double a, double b, double acc, double eps) {
-	double f2 = f(a + (b-a)/3);
-	double f3 = f(a + 2*(b-a)/3);
-	return integ_open(f, a, b, f2, f3, acc, eps);
+double adapt_open(function<double(double, int)> f, int p, double a, double b, double acc, double eps) {
+	double f2 = f(a + 2*(b-a)/6, p);
+	double f3 = f(a + 4*(b-a)/6, p);
+	return integ_open(f, p, a, b, f2, f3, acc, eps);
 }
 
-double integ_open(function<double(double)> f, double a, double b, double f2, double f3, double acc, double eps) {
-	double f1 = f(a + (b-a)/6);
-	double f4 = f(a + 5*(b-a)/6);
-	double Q = (f1 + f4) * (b-a) / 3 + (f2 + f3) * (b-a) / 6;
+double integ_open(function<double(double, int)> f, int p, double a, double b, double f2, double f3, double acc, double eps) {
+	double f1 = f(a + (b-a)/6, p);
+	double f4 = f(a + 5*(b-a)/6, p);
+	double Q = (2*f1 + f2 + f3 + 2*f4)*(b-a)/6;
 	double q = (f1 + f2 + f3 + f4) * (b-a) / 4;
 	double tol = acc + abs(Q)*eps;
 	double err = abs(Q-q)/2;
@@ -23,6 +24,29 @@ double integ_open(function<double(double)> f, double a, double b, double f2, dou
 		return Q;
 	} else {
 		acc /= sqrt(2);
-		return (integ_open(f, a, (a+b)/2, f1, f2, acc, eps) + integ_open(f, (a+b)/2, b, f3, f4, acc, eps));
+		return (integ_open(f, p, a, (a+b)/2, f1, f2, acc, eps) + integ_open(f, p, (a+b)/2, b, f3, f4, acc, eps));
+	}
+}
+
+double adapt_closed(function<double(double, int)> f, int p, double a, double b, double acc, double eps) {
+	double f1 = f(a, p);
+	double f3 = f((a+b)/2, p);
+	double f5 = f(b, p);
+	return integ_closed(f, p, a, b, f1, f3, f5, acc, eps);
+}
+
+double integ_closed(function<double(double, int)> f, int p, double a, double b, double f1, double f3, double f5, double acc, double eps) {
+	double f2 = f(a + (b-a)/4, p);
+	double f4 = f(a + 3*(b-a)/4, p);
+	double Q = ((f1 + f5)/8 + (f2 + f3 + f4)/4)*(b-a);
+	double q = ((f1 + f5)/4 + f3/2)*(b-a);
+	double tol = acc + abs(Q)*eps;
+	double err = abs(Q-q)/2;
+
+	if (err < tol) {
+		return Q;
+	} else {
+		acc /= sqrt(2);
+		return (integ_closed(f, p, a, (a+b)/2, f1, f2, f3, acc, eps) + integ_closed(f, p, (a+b)/2, b, f3, f4, f5, acc, eps));
 	}
 }
--- integration/mainA.cpp
+++ integration/mainA.cpp
@@ -3,21 +3,35 @@
 #include <functional>
 using namespace std;
 
-double adapt(function<double(double)> f, double a, double b, double eps, double acc);
+double adapt_open(function<double(double, int)> f, int p, double a, double b, double eps, double acc);
+double adapt_closed(function<double(double, int)> f, int p, double a, double b, double eps, double acc);
 
 int main() {
 int ncalls = 0;
-	function<double(double)> f = [&ncalls] (double x) {
+	function<double(double, int)> f = [&ncalls] (double x, int p) {
 		ncalls++;
-		return 4 * sqrt(1 - (1-x)*(1-x));
+		switch(p) {
+			case 1:
+				return sqrt(x);
+			case 2:
+				return 1/sqrt(x);
+			case 3:
+				return log(x)/sqrt(x);
+			case 4:
+				return 4*sqrt(1 - (1-x)*(1-x));
+			default:
+				throw invalid_argument("Function not defined");
+		}
 	};
 
 	double a = 0, b = 1, acc = 1e-5, eps = 1e-5;
+	int p = 1;
 
-	double Q = adapt(f, a, b, eps, acc);
+	double Q1 = adapt_open(f, p, a, b, eps, acc);
+	double Q2 = adapt_closed(f, p, a, b, eps, acc);
 
-	cout << "f(0.5) is: " << f(0.5) << endl;
-	cout << "Q is: " << Q << endl;
+	cout << "Q is: " << Q1 << endl;
+	cout << "Q is: " << Q2 << endl;
 	cout << "Number of f calls: " << ncalls << endl;
 return 0;
 }
