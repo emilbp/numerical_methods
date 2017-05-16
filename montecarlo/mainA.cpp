e5166223f1f3461eaf1249ddf61336a90c77ca8f
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-05-16 13:51:05 +0200

    Added examples

--- montecarlo/mainA.cpp
+++ montecarlo/mainA.cpp
@@ -32,7 +32,42 @@ int main() {
 	double Q, E;
 	tie(Q, E) = mc(f1, a, b, N);
 
-	cout << "Q is: " << Q << endl;
-	cout << "E is: " << E << endl;
+	cout << "Plain Monte Carlo integration of function: z = 1 if (x < 1 and y < 1)" << endl;
+	cout << "For x, y from 0 to 1" << endl;
+	cout << "Number of sampling points: " << N << endl;
+	cout << "Integral: " << Q << endl;
+	cout << "Exact ans: " << ans << endl;
+	cout << "Est. error: " << E << endl;
+	cout << "Actual error: " << abs(Q-ans) << endl << endl;
+
+	a = {-1, -1, 0}, b = {1, 1, 1};
+	ans = M_PI;
+	N = 100000;
+
+	tie(Q, E) = mc(f2, a, b, N);
+
+	cout << "Plain Monte Carlo integration of function: z = 2z if (x^2 + y^2 < 1 and z > 0)" << endl;
+	cout << "For x = {-1, -1, 0} to {1, 1, 1}" << endl;
+	cout << "Number of sampling points: " << N << endl;
+	cout << "Integral: " << Q << endl;
+	cout << "Exact ans: " << ans << endl;
+	cout << "Est. error: " << E << endl;
+	cout << "Actual error: " << abs(Q-ans) << endl << endl;
+
+
+	a = {0, 0, 0}, b = {M_PI, M_PI, M_PI};
+	ans = 1.3932039296856768591842462603255;
+	N = 100000;
+
+	tie(Q, E) = mc(f3, a, b, N);
+
+	cout << "Plain Monte Carlo integration of function:" << endl;
+	cout << "1 / (1 - cos(x)*cos(y)*cos(z)) / pi^3" << endl;
+	cout << "For x = {0, 0, 0} to {pi, pi, pi}" << endl;
+	cout << "Number of sampling points: " << N << endl;
+	cout << "Integral: " << Q << endl;
+	cout << "Exact ans: " << ans << endl;
+	cout << "Est. error: " << E << endl;
+	cout << "Actual error: " << abs(Q-ans) << endl << endl;
 return 0;
 }
