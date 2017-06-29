a818065031c87cb3219f6b6f38ec647153ff0627
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-06-29 13:03:45 +0200

    Initial complex function example

--- exam/mainA.cpp
+++ exam/mainA.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
-#include <iomanip>
 #include <complex>
 #include <cmath>
+#include <functional>
 
-std::complex<double> I0(std::complex<double> z2) {
-	std::complex<double> ans;
-	ans = z2+(3.0 + one_i);
-	return ans;
-}
+using namespace std;
+typedef complex<double> dcomp;
 
-int main()
-{
-	typedef std::complex<double> dcomp;    // your typedef
-	//using namespace std::literals;
-	std::cout << std::fixed << std::setprecision(1);
+main() {
+	dcomp i = -1; i = sqrt(i);
+	double pi = 2 * asin(1);
 
-	dcomp one_i(0.0, 1.0);
-	dcomp z = 1. - 2.*one_i;
-	std::cout << "z: " << z << std::endl;
-	std::cout << "I0(z): " << I0(z) << std::endl;
+	dcomp a = exp(2*pi*i);
+	cout << "i is " << i << " and Euler was right: e(i pi) = " << a << endl;
 
+	a = 2. + 2.*i;
+	dcomp b = 1. + 0.5*i;
+	cout << "a is " << a << endl;
+	cout << "b is " << b << endl;
+	cout << "a*b is " << a*b << endl;
 
-/*
-    dcomp z1 = one_i * one_i;     // imaginary unit squared
-    std::cout << "i * i = " << z1 << '\n';
-	
-    dcomp z2 = std::pow(one_i, 2); // imaginary unit squared
-    std::cout << "pow(i, 2) = " << z2 << '\n';
-	
-    double PI = std::acos(-1);
-    dcomp z3 = std::exp(one_i * PI); // Euler's formula
-    std::cout << "exp(i, pi) = " << z3 << '\n';
-	
-    dcomp two_i(0.0, 2.0);					     
-    dcomp z4 = 1. + two_i, z5 = 1. - two_i; // conjugates
-    std::cout << "(1+2i)*(1-2i) = " << z4*z5 << '\n';
-*/
+	function<dcomp(dcomp)> f = [] (dcomp z) {
+		return 2.*z;
+	};
+
+	cout << endl << "a is " << a << endl;
+	cout << "b is " << b << endl;
+	cout << "f(a) is " << f(a) << endl;
+	cout << "f(a)*b is " << f(a)*b << endl;
+	cout << "b*conj(b) is " << b*conj(b) << endl;
 }
