14d955fd0d4ef54a7739ce4379349cf39d3ba950
Emil Bjerglund Pedersen <emilbp@inano.au.dk>
2017-06-29 16:24:55 +0200

    Adaptive integrator working

--- exam/Readme.md
+++ exam/Readme.md
@@ -1,4 +1,8 @@
 # Exam project in Numerical Methods
 
-## Project (7/20)
+## Project 20: Adaptive integration of complex-valued functions
+
+*Implement an adaptive integrator which calculates the integral of a complex-valued function `f(z)` of a complex variable `z` along a straight line between two points in the complex plane.*
+
+
 
--- exam/mainA.cpp
+++ exam/mainA.cpp
@@ -10,32 +10,52 @@ dcomp adapt(function<dcomp(dcomp)> f, dcomp a, dcomp b, double acc, double eps);
 
 main() {
 	dcomp i = -1; i = sqrt(i);
-	double pi = 2 * asin(1), acc = 1e-5, eps = 1e-5;
+	double pi = 2 * asin(1);
 
+	// Define integration tolerance
+	double acc = 1e-5, eps = 1e-5;
+
+	// Define functions to integrate
 	function<dcomp(dcomp)> f = [] (dcomp z) {
 		return z;
 	};
 	function<dcomp(dcomp)> f2 = [] (dcomp z) {
 		return z*z;
 	};
+	function<dcomp(dcomp)> f3 = [] (dcomp z) {
+		return 1./z;
+	};
+	function<dcomp(dcomp)> f4 = [] (dcomp z) {
+		return exp(z);
+	};
 
-	dcomp a = 0. + 0.*i, b = 1. + i, res;
+	cout << "#####################\n# Numerical methods #\n#  Exam project 20  #\n# Emil T. Bjerglund #\n#####################" << endl;
 
+	cout << endl << "Adaptive integration of complex functions" << endl << endl;
+
+	// Define endpoints (a,b) of a line in the complex plane to integrate over
+	dcomp a = 1. + i, b = 3. + 3.*i;
+	dcomp res;
+	res = adapt(f, a, b, acc, eps);
+	cout << "f1(z) = z" << endl;
+	cout << "a = " << real(a) << "+" << imag(a) << "i" << ", b = " << real(b) << "+" << imag(b) << "i" << endl;
+	cout << "Integral of f1 from a to b = " << real(res) << " + " << imag(res) << "i" << endl << endl;
+
+	a = 0. + 0.*i, b = 1. + 1.*i;
 	res = adapt(f2, a, b, acc, eps);
+	cout << "f2(z) = z^2" << endl;
+	cout << "a = " << real(a) << "+" << imag(a) << "i" << ", b = " << real(b) << "+" << imag(b) << "i" << endl;
+	cout << "Integral of f2 from a to b = " << real(res) << " + " << imag(res) << "i" << endl << endl;
 
-	cout << "Res is: " << res << endl;
+	a = 1. + 0.*i, b = 2. - 1.*i;
+	res = adapt(f3, a, b, acc, eps);
+	cout << "f3(z) = 1/z" << endl;
+	cout << "a = " << real(a) << "+" << imag(a) << "i" << ", b = " << real(b) << "+" << imag(b) << "i" << endl;
+	cout << "Integral of f3 from a to b = " << real(res) << " + " << imag(res) << "i" << endl << endl;
 
-
-/*
-	dcomp a = 2. + 2.*i;
-	dcomp b = 1. + 0.5*i;
-	cout << "a is " << a << endl;
-	cout << "b is " << b << endl;
-	cout << "a*b is " << a*b << endl;
-	cout << endl << "a is " << a << endl;
-	cout << "b is " << b << endl;
-	cout << "f(a) is " << f2(a) << endl;
-	cout << "f(a)*b is " << f2(a)*b << endl;
-	cout << "b*conj(b) is " << b*conj(b) << endl;
-*/
+	a = 0. + 0.*i, b = 1. + 1.*i;
+	res = adapt(f4, a, b, acc, eps);
+	cout << "f4(z) = e^z" << endl;
+	cout << "a = " << real(a) << "+" << imag(a) << "i" << ", b = " << real(b) << "+" << imag(b) << "i" << endl;
+	cout << "Integral of f4 from a to b = " << real(res) << " + " << imag(res) << "i" << endl << endl;
 }
